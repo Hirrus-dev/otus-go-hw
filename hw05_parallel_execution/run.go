b9741f1322ef298a662fb5e637b154d0c03ab3be
Roman Gridnev <Gridnev-RA@sberbank.ru>
2024-06-05 21:01:52 +0300

    optimize

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 	"time"
 )
@@ -11,49 +10,39 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	if n < 1 {
-		return errors.New("Должна быть указана как минимум одна горутина")
+		return errors.New("должна быть указана как минимум одна горутина")
 	}
 	if m <= 0 {
 		return ErrErrorsLimitExceeded
 	}
-	var wg sync.WaitGroup
 	if n > len(tasks) {
 		n = len(tasks)
 	}
 	isErrorsLimitExceeded := false
 	maxErrCount := 0
-
-	//var maxErrorsCount = m
-	var workersCount = n
 	tasksChannel := make(chan Task)
 	resultsChannel := make(chan error)
+	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		for i := 0; i < len(tasks); i++ {
 			if maxErrCount >= m && m > 0 {
 				isErrorsLimitExceeded = true
 				break
-			} else {
-				tasksChannel <- tasks[i]
-				time.Sleep(time.Millisecond)
 			}
-
+			tasksChannel <- tasks[i]
+			time.Sleep(time.Millisecond)
 		}
 		close(tasksChannel) // закрываем канал по завершении
-		fmt.Println("===channel closed===")
 		wg.Done()
 	}()
-	for i := 0; i < workersCount; i++ {
-		//i := i
+	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
-
 			for {
 				task, open := <-tasksChannel
-
 				if !open {
 					break
 				}
@@ -63,7 +52,6 @@ func Run(tasks []Task, n, m int) error {
 		}()
 	}
 	go func() {
-
 		for {
 			result, open := <-resultsChannel
 			if !open {
